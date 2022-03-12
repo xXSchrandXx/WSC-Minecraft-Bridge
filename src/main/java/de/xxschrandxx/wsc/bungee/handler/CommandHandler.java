6d653c39100deea48dc64dcfb87bba81fec6ef24
xXSchrandXx <marvinschrand@yahoo.de>
2022-03-12 16:49:15 +0100

    Update CommandHandler.java

--- src/main/java/de/xxschrandxx/wsc/bungee/handler/CommandHandler.java
+++ src/main/java/de/xxschrandxx/wsc/bungee/handler/CommandHandler.java
@@ -4,39 +4,50 @@ import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
-import java.util.logging.Level;
 
-import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
 import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
 
-import de.xxschrandxx.wsc.bungee.MinecraftLinkerBungee;
+import de.xxschrandxx.wsc.bungee.api.AbstractHttpHandler;
+import de.xxschrandxx.wsc.bungee.api.MinecraftLinkerCommandSender;
 
-public class CommandHandler implements HttpHandler {
-
-    private final Gson gson = new Gson();
+public class CommandHandler extends AbstractHttpHandler {
 
     @Override
-    public void handle(HttpExchange exchange) throws IOException {
+    public HashMap<String, Object> run(HttpExchange exchange) {
         HashMap<String, Object> response = new HashMap<String, Object>();
-
-        // TODO
-        byte[] requestBytes = exchange.getRequestBody().readAllBytes();
-        String request = new String(requestBytes, StandardCharsets.UTF_8);
-
-        MinecraftLinkerBungee.getInstance().getLogger().log(Level.INFO, request);
-
-        String json = this.gson.toJson(response);
-        byte[] jsonBytes = json.getBytes();
-        try {
-            exchange.getResponseHeaders().add("Content-Type", "application/json");
-            exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, jsonBytes.length);
-            exchange.getResponseBody().write(jsonBytes);
-            exchange.close();
+        if (!exchange.getRequestMethod().equalsIgnoreCase("post")) {
+            response.put("status", "Method not allowed.");
+            response.put("statusCode", HttpURLConnection.HTTP_BAD_METHOD);
+            return response;
         }
-        catch (IOException e) {
+        try {
+            byte[] requestBytes = exchange.getRequestBody().readAllBytes();
+            String requestString = new String(requestBytes, StandardCharsets.UTF_8);
+            HashMap<String, String> request = this.gson.fromJson(requestString, typeStringString);
+            String command = request.get("command");
+            MinecraftLinkerCommandSender sender = new MinecraftLinkerCommandSender();
+            sender.setIP(exchange.getRemoteAddress().getAddress());
+            if (sender.dispatchCommand(command)) {
+                response.put("response", sender.flush());
+                response.put("status", "OK");
+                response.put("statusCode", HttpURLConnection.HTTP_OK);
+            }
+            else {
+                response.put("status", "Command not found.");
+                response.put("statusCode", HttpURLConnection.HTTP_INTERNAL_ERROR);
+            }
+        }
+        catch (JsonSyntaxException e) {
+            response.put("status", "JsonSyntaxException. " + e.getMessage());
+            response.put("statusCode", HttpURLConnection.HTTP_BAD_REQUEST);
             e.printStackTrace();
         }
+        catch (IOException e) {
+            response.put("status", "Internal Server Error.");
+            response.put("statusCode", HttpURLConnection.HTTP_INTERNAL_ERROR);
+            e.printStackTrace();
+        }
+        return response;
     }
-    
 }
