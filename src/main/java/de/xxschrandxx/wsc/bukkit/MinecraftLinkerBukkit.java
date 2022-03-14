49811054ea78b76396462e0c231d98851311155c
xXSchrandXx <marvinschrand@yahoo.de>
2022-03-14 18:56:07 +0100

    Start Bukkit/Spigot version

--- src/main/java/de/xxschrandxx/wsc/bukkit/MinecraftLinkerBukkit.java
+++ src/main/java/de/xxschrandxx/wsc/bukkit/MinecraftLinkerBukkit.java
@@ -1,6 +1,79 @@
 package de.xxschrandxx.wsc.bukkit;
 
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class MinecraftLinkerBukkit extends JavaPlugin {
+import de.xxschrandxx.wsc.core.IMinecraftLinker;
+import de.xxschrandxx.wsc.core.MinecraftLinkerHandler;
+
+public class MinecraftLinkerBukkit extends JavaPlugin implements IMinecraftLinker<CommandSender> {
+
+    private static MinecraftLinkerBukkit instance;
+
+    public static MinecraftLinkerBukkit getInstance() {
+        return instance;
+    }
+
+    // start of handler part
+    private MinecraftLinkerHandler handler;
+
+    @Override
+    public MinecraftLinkerHandler getHandler() {
+        return handler;
+    }
+
+    @Override
+    public boolean setHandler(CommandSender sender) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public void startHandler(CommandSender sender) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void stopHandler(CommandSender sender) {
+        // TODO Auto-generated method stub
+    }
+    // end of handler part
+
+    // start of plugin part
+    @Override
+    public void onEnable() {
+        // TODO
+    }
+
+    @Override
+    public void onDisable() {
+        // TODO
+    }
+    // end of plugin part
+
+    // start of config part
+    @Override
+    public Object getConfiguration() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public boolean reloadConfiguration() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean saveConfiguration() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean checkConfiguration(String path, Object def) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+    // end of config part
 }
---
+++ src/main/java/de/xxschrandxx/wsc/bukkit/handler/StatusHandler.java
@@ -0,0 +1,14 @@
+package de.xxschrandxx.wsc.bukkit.handler;
+
+import java.util.HashMap;
+
+import com.sun.net.httpserver.HttpExchange;
+
+import de.xxschrandxx.wsc.bukkit.api.AbstractBukkitHttpHandler;
+
+public class StatusHandler extends AbstractBukkitHttpHandler {
+    @Override
+    protected HashMap<String, Object> run(HttpExchange exchange) {
+        return null;
+    }
+}
