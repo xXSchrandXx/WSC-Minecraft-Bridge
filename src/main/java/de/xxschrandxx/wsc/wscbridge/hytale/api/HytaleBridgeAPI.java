3168efbae7517456379a1be8772c0c1e5a540f00
xXSchrandXx <marvinschrand@yahoo.de>
2026-01-22 19:55:20 +0100

    Fix #4

--- src/main/java/de/xxschrandxx/wsc/wscbridge/hytale/api/HytaleBridgeAPI.java
+++ src/main/java/de/xxschrandxx/wsc/wscbridge/hytale/api/HytaleBridgeAPI.java
@@ -2,10 +2,17 @@ package de.xxschrandxx.wsc.wscbridge.hytale.api;
 
 import java.util.ArrayList;
 import java.util.UUID;
+import java.util.concurrent.CancellationException;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
+import com.hypixel.hytale.component.Ref;
+import com.hypixel.hytale.component.Store;
 import com.hypixel.hytale.server.core.NameMatching;
+import com.hypixel.hytale.server.core.entity.entities.Player;
 import com.hypixel.hytale.server.core.universe.PlayerRef;
 import com.hypixel.hytale.server.core.universe.Universe;
+import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
 
 import de.xxschrandxx.wsc.wscbridge.core.IBridgePlugin;
 import de.xxschrandxx.wsc.wscbridge.core.api.BridgeCoreAPI;
@@ -28,27 +35,49 @@ public class HytaleBridgeAPI extends BridgeCoreAPI {
 
     @Override
     public ISender<?> getSender(UUID uuid, IBridgePlugin<?> instance) {
-        PlayerRef player = Universe.get().getPlayer(uuid);
-        if (player == null) {
+        PlayerRef playerRef = Universe.get().getPlayer(uuid);
+        if (playerRef == null) {
             return null;
         }
-        return new SenderHytale(player, instance);
+        return new SenderHytale(getPlayer(playerRef), instance);
     }
 
     @Override
     public ISender<?> getSender(String name, IBridgePlugin<?> instance) {
-        PlayerRef player = Universe.get().getPlayer(name, NameMatching.EXACT);
-        if (player == null) {
+        PlayerRef playerRef = Universe.get().getPlayer(name, NameMatching.EXACT);
+        if (playerRef == null) {
             return null;
         }
-        return new SenderHytale(player, instance);
+        return new SenderHytale(getPlayer(playerRef), instance);
+    }
+
+    public Player getPlayer(PlayerRef playerRef) {
+        Ref<EntityStore> ref = playerRef.getReference();
+        if (ref == null || !ref.isValid()) {
+            throw new IllegalArgumentException();
+        };
+
+        Store<EntityStore> store = ref.getStore();
+
+        CompletableFuture<Player> f = new CompletableFuture<Player>();
+
+        store.getExternalData().getWorld().execute(() -> {
+            Player playerComponent = (Player) store.getComponent(ref, Player.getComponentType());
+            f.complete(playerComponent);
+        });
+
+        try {
+            return f.get();
+        } catch (CancellationException | ExecutionException | InterruptedException e) {
+        }
+        return null;
     }
 
     @Override
     public ArrayList<ISender<?>> getOnlineSender(IBridgePlugin<?> instance) {
         ArrayList<ISender<?>> sender = new ArrayList<ISender<?>>();
-        for (PlayerRef player : Universe.get().getPlayers()) {
-            sender.add(new SenderHytale(player, instance));
+        for (PlayerRef playerRef : Universe.get().getPlayers()) {
+            sender.add(new SenderHytale(getPlayer(playerRef), instance));
         }
         return sender;
     }
--- src/main/java/de/xxschrandxx/wsc/wscbridge/hytale/api/command/SenderHytale.java
+++ src/main/java/de/xxschrandxx/wsc/wscbridge/hytale/api/command/SenderHytale.java
@@ -1,18 +1,15 @@
 package de.xxschrandxx.wsc.wscbridge.hytale.api.command;
 
+import java.awt.Color;
 import java.util.UUID;
 
+import com.hypixel.hytale.server.core.Message;
 import com.hypixel.hytale.server.core.command.system.CommandSender;
 import com.hypixel.hytale.server.core.entity.entities.Player;
 import com.hypixel.hytale.server.core.universe.PlayerRef;
-import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
 
 import de.xxschrandxx.wsc.wscbridge.core.IBridgePlugin;
 import de.xxschrandxx.wsc.wscbridge.core.api.command.ISender;
-
-import com.hypixel.hytale.component.Ref;
-import com.hypixel.hytale.component.Store;
-import com.hypixel.hytale.server.core.Message;
 
 public class SenderHytale implements ISender<CommandSender> {
 
@@ -21,22 +18,6 @@ public class SenderHytale implements ISender<CommandSender> {
 
     public SenderHytale(CommandSender sender, IBridgePlugin<?> instance) {
         this.sender = sender;
-        this.instance = instance;
-    }
-
-    public SenderHytale(PlayerRef playerRef, IBridgePlugin<?> instance) {
-        Ref<EntityStore> ref = playerRef.getReference();
-        if (ref == null || !ref.isValid()) {
-            throw new IllegalArgumentException();
-        };
-
-        Store<EntityStore> store = ref.getStore();
-        Player player = (Player) store.getComponent(ref, Player.getComponentType());
-        if (player == null) {
-            throw new IllegalArgumentException();
-        }
-
-        this.sender = (CommandSender) player;
         this.instance = instance;
     }
 
@@ -73,7 +54,101 @@ public class SenderHytale implements ISender<CommandSender> {
     }
 
     public void sendMessage(String text, String hoverMessage, String copyText) {
-        Message message = Message.raw(text);
+        Message message = Message.empty();
+        String[] parts = text.split("&");
+        Message lastPart = null;
+        for (String part : parts) {
+            if (part.isEmpty()) {
+                continue;
+            }
+            Message messagePart = Message.empty();
+            if (lastPart != null) {
+                messagePart.color(lastPart.getColor());
+//                messagePart.bold(lastPart.isBold());
+//                messagePart.italic(lastPart.isItalic());
+//                messagePart.monospace(lastPart.isMonospace());
+            }
+            if (part.isBlank()) {
+                message.insert(part);
+                continue;
+            }
+            char first = part.charAt(0);
+            switch (first) {
+                case '0':
+                    messagePart.color(new Color(0));
+                    break;
+                case '1':
+                    messagePart.color(new Color(170));
+                    break;
+                case '2':
+                    messagePart.color(new Color(43520));
+                    break;
+                case '3':
+                    messagePart.color(new Color(43690));
+                    break;
+                case '4':
+                    messagePart.color(new Color(11141120));
+                    break;
+                case '5':
+                    messagePart.color(new Color(11141290));
+                    break;
+                case '6':
+                    messagePart.color(new Color(16755200));
+                    break;
+                case '7':
+                    messagePart.color(new Color(11184810));
+                    break;
+                case '8':
+                    messagePart.color(new Color(5592405));
+                    break;
+                case '9':
+                    messagePart.color(new Color(5592575));
+                    break;
+                case 'a':
+                    messagePart.color(new Color(5635925));
+                    break;
+                case 'b':
+                    messagePart.color(new Color(5636095));
+                    break;
+                case 'c':
+                    messagePart.color(new Color(16733525));
+                    break;
+                case 'd':
+                    messagePart.color(new Color(16733695));
+                    break;
+                case 'e':
+                    messagePart.color(new Color(16777045));
+                    break;
+                case 'f':
+                    messagePart.color(new Color(16777215));
+                    break;
+                case 'k':
+                    // unsupported
+                    break;
+                case 'l':
+                    messagePart.bold(true);
+                    break;
+                case 'm':
+                    // unsupported
+                    break;
+                case 'n':
+                    // unsupported
+                    break;
+                case 'o':
+                    messagePart.italic(true);
+                    break;
+                case 'r':
+                    messagePart.color(Color.WHITE);
+                    messagePart.bold(false);
+                    messagePart.italic(false);
+                    messagePart.monospace(false);
+                    break;
+            }
+            messagePart.insert(part.substring(1));
+            lastPart = messagePart;
+            message.insert(messagePart);
+        }
+
         this.sender.sendMessage(message);
     }
 
